d34631893bd0ede570ce8488373de2de9e006137
Michael Schwanzer <michael.schwanzer@gmail.com>
2023-10-24 10:51:08 -0400

    formatting and robustness, custom font

--- generate_badges.py
+++ generate_badges.py
@@ -8,7 +8,8 @@ from PyPDF2 import PdfReader, PdfWriter
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import inch
 import qrcode
-
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 
 # make sure to only use the google spreadsheet link before the /edit
 SPREADSHEET_URL = 'GOOGLE_SPREADSHEET_LINK' + '/gviz/tq?tqx=out:csv'  # Append to get CSV export
@@ -20,10 +21,24 @@ OUTPUT_PDF_PATH = 'output.pdf'
 data = pd.read_csv(SPREADSHEET_URL)
 #print(data)
 
-overlay_file_path = 'overlay.pdf'
-c = canvas.Canvas(overlay_file_path, pagesize=letter)
-page_width, page_height = letter  # Assuming you're using letter-sized paper. Adjust as needed.
 
+font_name_bold = "BC Sans Bold"
+font_path_bold = "/Users/mschwanzer/Library/Fonts/2023_01_01_BCSans-Bold_2f.ttf"  # Replace with the actual path you found
+pdfmetrics.registerFont(TTFont(font_name_bold, font_path_bold))
+
+font_name_regular = "BC Sans Regular"
+font_path_regular = "/Users/mschwanzer/Library/Fonts/2023_01_01_BCSans-Regular_2f.ttf"  # Replace with the actual path you found
+pdfmetrics.registerFont(TTFont(font_name_regular, font_path_regular))
+
+
+with open(TEMPLATE_PDF_PATH, 'rb') as file:
+    reader = PdfReader(file)
+    page = reader.pages[0]
+    page_width, page_height =  float(page.mediabox.width), float(page.mediabox.height)
+print(f"Template Width: {page_width}, Height: {page_height}")
+
+overlay_file_path = 'overlay.pdf'
+c = canvas.Canvas(overlay_file_path, pagesize=(page_width, page_height))
 def draw_centered_string(c, text, x, y):
     text_width = c.stringWidth(text, 'Helvetica', 18)  # Assuming font Helvetica size 14; adjust if different
     new_x = (page_width - text_width) / 2
@@ -44,28 +59,34 @@ with open(TEMPLATE_PDF_PATH, 'rb') as template_file:
     print("ready to iterate over lines")
     # Iterate over rows in spreadsheet and overlay information
     for _, row in data.iterrows():
-        y = page_height / 2  # Starting y-coordinate
-        x = 300
         template = PdfReader(template_file)
-        firstname, lastname, pronouns, role, company, link = row['firstname'], row['lastname'], row['pronouns'], row['role'], row['company'], row['link']
-        print(f"f: {firstname}, l: {lastname}")
+        firstname, lastname, company, link = row['firstname'], row['lastname'], row['company'], str(row['link'])
+        print(f"f: {firstname}, l: {lastname}, c: {company}, l: {link}")
         # Create overlay with data
         overlay_file_path = 'overlay.pdf'
         c = canvas.Canvas(overlay_file_path)
-        c.setFont("Helvetica", 20)  # Set font to Helvetica and size to 14
+        
         c.setFillColorRGB(0, 0, 0)  # Set color to black
 
-        # Drawing the name and pronouns
-        y = draw_centered_string(c, f"{firstname} {lastname} ({pronouns})", x, y)
-
-        # Drawing the role
-        y = draw_centered_string(c, role, x, y)
+        # Drawing the name
+        text = f"{firstname} {lastname}"
+        c.setFont("BC Sans Bold", 17)  # name BC Sans (Bold), size 17, organisation BC Sans (Regular), size 14
+        text_width = c.stringWidth(text, 'BC Sans Bold', 17) 
+        new_x = (page_width - text_width) / 2
+        c.drawString(new_x, page_height / 2, text)
 
         # Drawing the company
-        y = draw_centered_string(c, company, x, y)
+        c.setFont("BC Sans Regular", 14)  # name BC Sans (Bold), size 17, organisation BC Sans (Regular), size 14
+        text = f"{company}"
+        c.setFont("BC Sans Regular", 14)  # name BC Sans (Bold), size 17, organisation BC Sans (Regular), size 14
+        text_width = c.stringWidth(text, 'BC Sans Regular', 14) 
+        new_x = (page_width - text_width) / 2
+        c.drawString(new_x, page_height / 2 - 20, text)
         
-        qr_file_path = generate_qr(link)
-        c.drawImage(qr_file_path, (page_width - 100) / 2, y - 100, width=100, height=100)  # Adjust coordinates and size as needed
+        if "http" in link: 
+            qr_file_path = generate_qr(link)
+            #print(f"width = {page_width}, height = {page_height}")
+            c.drawImage(qr_file_path, page_width - 61.5, 58, width=27, height=27)  # Adjust coordinates and size as needed
 
         c.save()
 
